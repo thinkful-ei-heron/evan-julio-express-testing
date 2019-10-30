ff98c59f6b3f7658566f5e79495c37539023d6cd
julio-crypt0 <hernandezjulio58@ymail.com>
2019-10-30 12:21:29 -0700

    passing 5 tests

--- test/app.test.js
+++ test/app.test.js
@@ -7,6 +7,93 @@ describe('GET /apps', () => {
     return supertest(app)
       .get('/apps')
       .expect(200)
-      .expect('Content-Type', /json/);
+      .expect('Content-Type', /json/)
+      .then((res) => {
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf.at.least(1);
+        const app = res.body[0];
+        expect(app).to.include.all.keys(
+          'App',
+          'Category',
+          'Reviews',
+          'Size',
+          'Installs',
+          'Type',
+          'Price',
+          'Genres'
+        );
+      });
+  });
+  it('should be 400 if sort is incorrect', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: 'MISTAKE' })
+      .expect(400, 'Sort must be one of App title or Rating');
+  });
+  it('should sort by title', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: 'App' })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then((res) => {
+        expect(res.body).to.be.an('array');
+        let sorted = true;
+
+        let i = 0;
+        // iterate once less than the length of the array
+        // because we're comparing 2 items in the array at a time
+        while (i < res.body.length - 1) {
+          // compare book at `i` with next book at `i + 1`
+          const AppAtI = res.body[i];
+          const AppAtIPlus1 = res.body[i + 1];
+          // if the next App is less than the App at i,
+          if (AppAtIPlus1.title < AppAtI.title) {
+            // the Apps were not sorted correctly
+            sorted = false;
+            break; // exit the loop
+          }
+          i++;
+        }
+        expect(sorted).to.be.true;
+      });
+  });
+
+  it('should sort by Rating', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: 'Rating' })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then((res) => {
+        expect(res.body).to.be.an('array');
+        let sorted = true;
+
+        let i = 0;
+        // iterate once less than the length of the array
+        // because we're comparing 2 items in the array at a time
+        while (i < res.body.length - 1) {
+          // compare book at `i` with next book at `i + 1`
+          const AppAtI = res.body[i];
+          const AppAtIPlus1 = res.body[i + 1];
+          // if the next App is less than the App at i,
+          if (AppAtIPlus1.Rating < AppAtI.Rating) {
+            // the Apps were not sorted correctly
+            sorted = false;
+            break; // exit the loop
+          }
+          i++;
+        }
+        expect(sorted).to.be.true;
+      });
+  });
+  it('should return 400 if genre incorrect', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ genre: 'MISTAKE' })
+      .expect(
+        400,
+        'Genre must be Action, Puzzle, Strategy, Casual, Arcade, Card'
+      );
   });
 });
